73873ee830153cf0c58ebf834706dbe527375bc5
Елена Владимировна <50554917+Skylin44@users.noreply.github.com>
2024-10-03 09:08:13 +0300

    Add files via upload

---
+++ module_3_hard.py
@@ -0,0 +1,25 @@
+def calculate_structure_sum(var):
+    global summa
+    if isinstance(var, int):
+        summa += var
+    elif isinstance(var, str):
+        summa += len(var)
+    elif isinstance(var, (list, tuple, set)):
+        for i in var:
+            calculate_structure_sum(i)
+    elif isinstance(var, dict):
+        for key, value in var.items():
+            calculate_structure_sum(key)
+            calculate_structure_sum(value)
+
+
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+summa = 0
+result = calculate_structure_sum(data_structure)
+print(result)
